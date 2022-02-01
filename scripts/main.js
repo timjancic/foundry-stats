e0dae83e69f14cfe4566efc0149f5464d03bd292
timjancic <timjancic.gravity@gmail.com>
2022-01-31 20:55:07 -0600

    Parsing data, stuck on line 52 where it is assigning values it shoudln't even though the loops are running correctly

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 const diceTypes = ["d100","d20","d12","d10","d8","d6","d4","d2"];
 const dataNames = ["alias","flavor","advantageMode","nDice","faces","result"];
+const pcArray = ["Brotir","Berserker"];
+let pcStats = [...Array(pcArray.length)];
+pcStats.fill([]);
+console.log(pcStats);
 
 
 function readFile(input) {
@@ -10,17 +14,26 @@ function readFile(input) {
   reader.readAsText(file);
 
   reader.onload = function() {
-
     let rawArray = reader.result.split("\n");
 
     let dataTable = new Array(rawArray.length);
+
+    //create a table of values from the info
     for (let i = 0; i < rawArray.length; i++) {
       dataTable[i] = rawArray[i].split(",");
+      //turn blank numbers into 0 and number strings into integers
+      for (let j = 2; j < dataTable[i].length; j++) {
+        if (dataTable[i][j] == "") {
+          dataTable[i][j] = 0;
+          continue;
+        }
+        dataTable[i][j] = parseInt(dataTable[i][j]);
+      }
     }
 
     console.log(rawArray);
     console.log(dataTable);
-    parseData(rawArray);
+    parseData(dataTable);
   };
 
   reader.onerror = function() {
@@ -29,7 +42,19 @@ function readFile(input) {
 
 }
 
-function parseData(rawArray) {
+function parseData(dataTable) {
+  for (let i = 0; i < dataTable.length; i++) {
+    for (let j = 0; j < pcArray.length; j++) {
+      //match rows with the same alias
+
+      if (dataTable[i][0] == pcArray[j]) {
+        console.log("dataTable[" + i + "][0]: " + dataTable[i][0] + " pcArray[" + j + "]: " + pcArray[j]);
+        pcStats[j].push(dataTable[i]);
+      }
+    }
+  }
+
+  /*
   count = 0;
   for (let i = 0; i < rawArray.length; i++) {
     if (rawArray[i].includes('Brotir')) {
@@ -37,6 +62,7 @@ function parseData(rawArray) {
     }
   }
   console.log("Brotir appears " + count + " times!");
+  */
 }
 
 function getRoll(rawSingle) {
