d73591860fb894d961b06892034fbab64f12bf7f
timjancic <timjancic.gravity@gmail.com>
2022-02-02 11:44:42 -0600

    Simplified creation of data table and changed the layout of the raw data. Beginning of makeProfilePC function

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,13 @@
 const diceTypes = ["d100","d20","d12","d10","d8","d6","d4","d2"];
-const dataNames = ["alias","flavor","advantageMode","nDice","faces","result"];
+const dataNames = ["alias","flavor","advantageMode","nDice","faces","result","total"];
 
 /*
-the stats (not data) for each alias is organized in a table as follows. Each name is a column of data
+the profile for each alias is organized in a table as follows. Each name is a column of data
 [ ["attack roll result", "attack name"],["damage roll result", "damage name"], ["skill result","skill name"], ["d20 data","advantage data"] ];
 for example:
 18, sword - 8, sword - 17
 */
-//const statNames = ["attack roll data", "attack names", "damage roll data", "damage names", "skill data", ]
-const pcArray = ["Brotir","Berserker"];
+const pcArray = ["Brotir","Zanna"];
 let pcData = new Array(pcArray.length);
 let dmData = [[]];
 let pcStats = new Array(pcArray.length);
@@ -29,8 +28,11 @@ function readFile(input) {
 
     //create a table of values from the info
     for (let i = 0; i < rawArray.length; i++) {
-      dataTable[i] = rawArray[i].split(",");
+      dataTable[i] = JSON.parse(rawArray[i]);
+
+      // dataTable[i] = rawArray[i].split(",");
       //turn blank numbers into 0 and number strings into integers
+      /*
       for (let j = 2; j < dataTable[i].length; j++) {
         if (dataTable[i][j] == "") {
           dataTable[i][j] = 0;
@@ -38,6 +40,7 @@ function readFile(input) {
         }
         dataTable[i][j] = parseInt(dataTable[i][j]);
       }
+      */
     }
 
     console.log(rawArray);
@@ -57,19 +60,26 @@ function assignData(dataTable) {
       //match rows with the same alias
       if (dataTable[i][0] == pcArray[j]) {
         pcData[j].push(dataTable[i]);
-      } else if (dataTable[i][0] != ""){
+        break; //break the for loop since there is only be one alias per row of data.
+      } else if (dataTable[i][0] != "" && j == pcArray.length - 1){
         dmData[0].push(dataTable[i]); //if alias exists but is not part of names array, assume it is DM controlled character
       }
     }
   }
-
 }
 
-function getRoll(rawSingle) {
-  let num = 0, dice = null, roll = 0, total = 0;
-  for (x in diceTypes) {
-    if (rawSingle.includes(x)) {
-      dice = x;
-    }
+function makeProfilePC(aliasData) {
+  /*
+  this function takes a single index of pcData and synthesizes the data into a profile
+  profile = [ ["attack roll result", "attack name"],["damage roll result", "damage name"], ["skill result","skill name"], ["d20 data","advantage data"] ];
+  aliasData = ["alias","flavor","advantageMode","nDice","faces","result"];
+  */
+  let attackData = [[],[]];
+  let damageData = [[],[]];
+  let skillData = [[],[]];
+  let d20Data = [[],[]];
+
+  for (let i = 0; i < aliasData.length; i++) {
+
   }
 }
